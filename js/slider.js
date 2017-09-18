f2b98bae85c064ae1626c75678586f0d916a4ebb
mjstobo <mjstobo@gmail.com>
2017-09-18 23:47:30 +1000

    Added slick slider

--- js/slider.js
+++ js/slider.js
@@ -1,1 +1,11 @@
 
+$(document).ready(function(){
+    $('.image').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 400,
+      pauseOnHover: true
+    });
+  });
