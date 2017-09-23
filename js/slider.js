3b1aa17d2dcfa36ab0daf7bbee3c205518149d3a
Matthew Stobo <mjstobo@Matthews-MacBook-Pro.local>
2017-09-23 16:02:50 +1000

    Added chevrons, removed slickArrows // added logic for next + back based on half of div

--- js/slider.js
+++ js/slider.js
@@ -1,11 +1,32 @@
-
-$(document).ready(function(){
-    $('.image').slick({
-      infinite: true,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 400,
-      pauseOnHover: true
-    });
+$(document).ready(function () {
+  $('#slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 400,
+    lazyLoad: true,
+    arrows:false,
+    pauseOnHover: true
   });
+});
+
+var mouseSide;
+
+$("#slider").on('mousemove', function (e) {
+  if ((e.pageX - this.offsetLeft) < $(this).width() / 2) {
+    $(".image").css('cursor', 'url(./fonts/icon-prev.png), auto');
+    mouseSide = 1;
+  } else {
+    $(".image").css('cursor', 'url(./fonts/icon-next.png), auto');
+    mouseSide = 0;
+  }
+});
+
+$("#slider").click(function () {
+  if (mouseSide == 1) {
+    $(this).slick('slickPrev');
+  } else {
+    $(this).slick('slickNext');
+  }
+})
